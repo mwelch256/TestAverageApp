3aaff26a27df06b8a42177940c8f06a2a5fec840
unknown <mwelch1194@gmail.com>
2019-09-06 14:40:23 -0500

    created a new project with new computer and new project name

---
+++ TestAverageApp/TestAverageApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace TestAverageApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What are your first test score");
+            var responseFromUser1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What are your second test score");
+            var responseFromUser2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("What are your third test score");
+            var responseFromUser3 = int.Parse(Console.ReadLine());
+
+            int average = (responseFromUser1 + responseFromUser2 + responseFromUser3) / 3;
+            Console.WriteLine($"your average is {average}");
+        }
+    }
+}
